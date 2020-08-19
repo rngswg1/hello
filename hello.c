f3f324a47a09a46e2d2c3a7026cbc7bfaedffce8
Chris Petersen <c.petersen@huskers.unl.edu>
2020-08-19 19:17:33 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Christopher Petersen
+ * Date:   2020/08/18
+ *
+ * A simple program in C " hello world"
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv){
+
+     printf("Chris Petersen\n");
+
+
+     return 0;
+
+ }
